e0bfe93921be651f6562c134e93ddb2190151b48
abel <abeldsouza12@gmail.com>
2026-04-10 10:58:33 +0530

    i think its final may

--- parish-frontend/src/components/PageTransition.jsx
+++ parish-frontend/src/components/PageTransition.jsx
@@ -15,22 +15,7 @@ export default function PageTransition({ children, delay = 100 }) {
 
   return (
     <div className="page-transition">
-<<<<<<< HEAD
-      {isLoading && (
-        <div className="page-loader">
-          <div className="loader-content">
-            <img 
-                src="/line.png" 
-              alt="loading" 
-              className="loader-animation" 
-            />
-          </div>
-        </div>
-      )}
-      <div className={`page-content ${!isLoading ? "visible" : ""}`}>
-=======
       <div className={`page-content ${isVisible ? "visible" : ""}`}>
->>>>>>> 177287f (i think its final)
         {children}
       </div>
     </div>
